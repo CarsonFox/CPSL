a9894f474120495c365676c47e05426701bde9cf
Carson Fox <carson.fox@aggiemail.usu.edu>
2020-02-24 16:32:45 -0700

    Should start using shared pointers by default

--- FrontEnd/ConstantFolding.hpp
+++ FrontEnd/ConstantFolding.hpp
@@ -1,1 +1,15 @@
 #pragma once
+
+#include <memory>
+
+#include "AST/Expressions/AllExpressions.hpp"
+
+#include <yorel/yomm2/cute.hpp>
+
+using yorel::yomm2::virtual_;
+
+register_class (Expression);
+register_class (LiteralExpression, Expression);
+register_class (BinaryOpExpression, Expression);
+
+//declare_method(std::unique_ptr<Expression>, fold, (virtual_<const Expression &>));
