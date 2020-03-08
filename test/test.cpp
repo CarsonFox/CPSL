def1f8001e7fb1b7d6b30b958685d9bb9d3dcfb7
Carson Fox <carson.fox@aggiemail.usu.edu>
2020-03-07 23:43:24 -0700

    Comparison operator constant folding tests

--- test/test.cpp
+++ test/test.cpp
@@ -133,3 +133,63 @@ BOOST_AUTO_TEST_CASE(fold_or) {
     auto f = OrExpression(new LiteralExpression(false), new LiteralExpression(false)).try_fold();
     BOOST_CHECK(f.has_value() && !*f);
 }
+
+BOOST_AUTO_TEST_CASE(fold_less) {
+    auto t = LessExpression(new LiteralExpression(1), new LiteralExpression(2)).try_fold();
+    BOOST_CHECK(t.has_value() && *t);
+
+    auto f = LessExpression(new LiteralExpression(3), new LiteralExpression(2)).try_fold();
+    BOOST_CHECK(f.has_value() && !*f);
+
+    f = LessExpression(new LiteralExpression(2), new LiteralExpression(2)).try_fold();
+    BOOST_CHECK(f.has_value() && !*f);
+}
+
+BOOST_AUTO_TEST_CASE(fold_less_equal) {
+    auto t = LessEqualExpression(new LiteralExpression(1), new LiteralExpression(2)).try_fold();
+    BOOST_CHECK(t.has_value() && *t);
+
+    auto f = LessEqualExpression(new LiteralExpression(3), new LiteralExpression(2)).try_fold();
+    BOOST_CHECK(f.has_value() && !*f);
+
+    t = LessEqualExpression(new LiteralExpression(2), new LiteralExpression(2)).try_fold();
+    BOOST_CHECK(t.has_value() && *t);
+}
+
+BOOST_AUTO_TEST_CASE(fold_greater) {
+    auto t = GreaterExpression(new LiteralExpression(2), new LiteralExpression(1)).try_fold();
+    BOOST_CHECK(t.has_value() && *t);
+
+    auto f = GreaterExpression(new LiteralExpression(3), new LiteralExpression(4)).try_fold();
+    BOOST_CHECK(f.has_value() && !*f);
+
+    f = GreaterExpression(new LiteralExpression(3), new LiteralExpression(3)).try_fold();
+    BOOST_CHECK(f.has_value() && !*f);
+}
+
+BOOST_AUTO_TEST_CASE(fold_greater_equal) {
+    auto t = GreaterEqualExpression(new LiteralExpression(1), new LiteralExpression(-10)).try_fold();
+    BOOST_CHECK(t.has_value() && *t);
+
+    auto f = GreaterEqualExpression(new LiteralExpression(2), new LiteralExpression(10)).try_fold();
+    BOOST_CHECK(f.has_value() && !*f);
+
+    t = GreaterEqualExpression(new LiteralExpression(3), new LiteralExpression(3)).try_fold();
+    BOOST_CHECK(t.has_value() && *t);
+}
+
+BOOST_AUTO_TEST_CASE(fold_equal) {
+    auto t = EqualExpression(new LiteralExpression(2), new LiteralExpression(2)).try_fold();
+    BOOST_CHECK(t.has_value() && *t);
+
+    auto f = EqualExpression(new LiteralExpression(2), new LiteralExpression(3)).try_fold();
+    BOOST_CHECK(f.has_value() && !*f);
+}
+
+BOOST_AUTO_TEST_CASE(fold_not_equal) {
+    auto t = NotEqualExpression(new LiteralExpression(2), new LiteralExpression(3)).try_fold();
+    BOOST_CHECK(t.has_value() && *t);
+
+    auto f = NotEqualExpression(new LiteralExpression(2), new LiteralExpression(2)).try_fold();
+    BOOST_CHECK(f.has_value() && !*f);
+}
