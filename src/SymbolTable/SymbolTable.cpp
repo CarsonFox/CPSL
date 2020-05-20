bb5e7b64f969b11b43ca0a8b55fb258e3a0a2298
Carson Fox <carson.fox@aggiemail.usu.edu>
2020-05-20 13:59:53 -0400

    Looks like I'll need types working, at least partially, before global variables.

--- src/SymbolTable/SymbolTable.cpp
+++ src/SymbolTable/SymbolTable.cpp
@@ -1,5 +1,20 @@
 #include "SymbolTable.hpp"
 
-void SymbolTable::addVariable(std::string id, std::shared_ptr<Type> type) {
+SymbolTable::SymbolTable() {
+    //Initialize global scope
+    scopes.emplace_back();
+}
 
+void SymbolTable::addVariable(std::string id, std::shared_ptr<Type> type) {
+    if (scopes.size() == 1) {
+        //Global variable
+        scopes[0].addVariable(id, type, "$gp");
+    } else {
+        //Local variable
+        scopes[1].addVariable(id, type, "$sp");
+    }
+}
+
+void SymbolTable::Scope::addVariable(std::string id, std::shared_ptr<Type> type, std::string base) {
+    variables[id] = Variable();
 }
--- src/SymbolTable/SymbolTable.hpp
+++ src/SymbolTable/SymbolTable.hpp
@@ -2,13 +2,27 @@
 
 #include <string>
 #include <memory>
+#include <unordered_map>
+#include <vector>
+
+#include "Variable.hpp"
 
 //I'm looking forward to modules so much
 struct Type;
 
 class SymbolTable {
+    struct Scope {
+        std::unordered_map<std::string, Variable> variables;
+
+        Scope() = default;
+
+        void addVariable(std::string, std::shared_ptr<Type>, std::string);
+    };
+
+    std::vector<Scope> scopes;
+
 public:
-    SymbolTable() = default;
+    SymbolTable();
 
     void addVariable(std::string, std::shared_ptr<Type>);
 };
